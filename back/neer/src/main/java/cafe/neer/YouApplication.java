1a3ef9825510f3dcd830bb224aaeb17d25b50685
강민승 <k4minseung@gmail.com>
2023-10-13 09:31:19 +0900

    ✨ feat : [spring io 프로젝트 생성] | [강민승] | [neer]

---
+++ back/neer/src/main/java/cafe/neer/YouApplication.java
@@ -0,0 +1,13 @@
+package cafe.neer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouApplication.class, args);
+	}
+
+}
