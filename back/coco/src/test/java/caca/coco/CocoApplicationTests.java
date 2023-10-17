6715d014f72c5d85f90844047e1d74c815b8900a
강민승 <k4minseung@gmail.com>
2023-10-17 11:06:55 +0900

    ✨ feat : [order system server] | [강민승] | [coco]

--- back/coco/src/test/java/caca/coco/CocoApplicationTests.java
+++ back/coco/src/test/java/caca/coco/CocoApplicationTests.java
@@ -1,10 +1,10 @@
-package cafe.neer;
+package caca.coco;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class YouApplicationTests {
+class CocoApplicationTests {
 
 	@Test
 	void contextLoads() {
