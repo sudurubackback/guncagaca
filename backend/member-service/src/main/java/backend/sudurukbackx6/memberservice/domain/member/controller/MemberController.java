5fcf871f1ec0fc1a040a484d2a1bd54dc1f80fd8
Giggle1998 <rlvy98@naver.com>
2023-10-20 17:15:00 +0900

    ✨ feat : [Member Controller 생성] | [이기표] | [MemberController.java]

--- backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/controller/MemberController.java
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/controller/MemberController.java
@@ -1,2 +1,18 @@
-package backend.sudurukbackx6.memberservice.domain.member.controller;public class MemberController {
+package backend.sudurukbackx6.memberservice.domain.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+public class MemberController {
+
+    /* TODO
+    카카오 소셜 로그인 구현
+     */
+    @GetMapping("/sign")
+    public Object sign() {
+        return "sign";
+    }
 }
