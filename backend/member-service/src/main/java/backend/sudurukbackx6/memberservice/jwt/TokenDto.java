1d9a29cbb779e053d26b3b83cd555258cbd4409e
Giggle1998 <rlvy98@naver.com>
2023-10-20 17:38:25 +0900

    ✨ feat : [Jwt  생성 및 관리 기능 개발] | [이기표] | [TokenProvider.java]

--- backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/jwt/TokenDto.java
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/jwt/TokenDto.java
@@ -1,2 +1,15 @@
-package backend.sudurukbackx6.memberservice.jwt;public class TokenDto {
+package backend.sudurukbackx6.memberservice.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+    public String token;
+    public Date tokenExpirationTime;
 }
