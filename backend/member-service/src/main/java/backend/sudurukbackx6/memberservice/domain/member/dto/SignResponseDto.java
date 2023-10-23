3611690616f5582721f33cca282ea7219a0ffe86
Giggle1998 <rlvy98@naver.com>
2023-10-23 17:38:20 +0900

    ✨ feat : [SignResponseDto 생성] | [이기표] | [SignResponseDto .java]

--- backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/dto/SignResponseDto.java
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/dto/SignResponseDto.java
@@ -1,2 +1,18 @@
-package backend.sudurukbackx6.memberservice.domain.member.dto;public class SignResponseDto {
+package backend.sudurukbackx6.memberservice.domain.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignResponseDto {
+    public String accessToken;
+    public String refreshToken;
+
+    @Builder
+    public SignResponseDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
 }
