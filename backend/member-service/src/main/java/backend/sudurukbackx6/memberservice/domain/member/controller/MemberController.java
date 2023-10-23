3499677f49739dd3d11feea4203d4dcfeccc62f5
Giggle1998 <rlvy98@naver.com>
2023-10-23 17:39:00 +0900

    ✨ feat : [회원관리 및 로그인 컨트롤러 개발] | [이기표] | [MemberController.java]

--- backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/controller/MemberController.java
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/controller/MemberController.java
@@ -1,18 +1,24 @@
 package backend.sudurukbackx6.memberservice.domain.member.controller;
 
+import backend.sudurukbackx6.memberservice.domain.member.dto.SignRequestDto;
+import backend.sudurukbackx6.memberservice.domain.member.dto.SignResponseDto;
+import backend.sudurukbackx6.memberservice.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/member")
+@RequiredArgsConstructor
 public class MemberController {
 
-    /* TODO
-    카카오 소셜 로그인 구현
-     */
+    private final MemberService memberService;
+    
     @GetMapping("/sign")
-    public Object sign() {
-        return "sign";
+    public ResponseEntity<SignResponseDto> getSign(@RequestBody SignRequestDto signRequestDto) {
+        return ResponseEntity.ok(memberService.getSign(signRequestDto));
     }
 }
