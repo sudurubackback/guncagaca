a1fae4a4f565ed0a01773c52191fab6a2b8e7e53
김제준 <hoilday5303@naver.com>
2023-10-23 14:39:14 +0900

    ✨ feat : 랜딩페이지 생성 | 김제준

---
+++ front/guncagaca/lib/landingpage.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+// import 'package:cafe2/loginpage.dart';
+
+class LandingPage extends StatefulWidget {
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3),() {
+      // Get.to(LoginPage());
+      // print("제대로 작동");
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xfff8e9d7),
+        child: Center(
+          child: Image.asset('assets/image/main_img.png'), // 이미지를 화면 가운데로 이동
+        ),
+      ),
+    );
+  }
+}
