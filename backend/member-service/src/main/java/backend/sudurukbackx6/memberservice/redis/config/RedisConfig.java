4024f8aabda5633cb9d236cf73ac34ee55279b90
Giggle1998 <rlvy98@naver.com>
2023-10-24 16:14:20 +0900

    🏗 chore : [Redis 관련 설정] | [이기표] | [RedisConfig, RedisProperties, RedisUtil]

---
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/redis/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package backend.sudurukbackx6.memberservice.redis.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@RequiredArgsConstructor
+@EnableRedisRepositories
+@Configuration
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());   //connection
+        redisTemplate.setKeySerializer(new StringRedisSerializer());    // key
+        redisTemplate.setValueSerializer(new StringRedisSerializer());  // value
+        return redisTemplate;
+    }
+
+}
---
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/redis/config/RedisProperties.java
@@ -0,0 +1,15 @@
+package backend.sudurukbackx6.memberservice.redis.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis")
+@Setter
+@Getter
+public class RedisProperties {
+    private int port;
+    private String host;
+}
