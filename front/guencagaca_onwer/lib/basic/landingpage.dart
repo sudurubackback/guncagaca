b30c095ac59373a1d7c22788493e6e0f6a51548e
Kwon_Min_Jae <knk598500@gmail.com>
2023-10-24 12:53:45 +0900

    ✨ feat : [회원가입 화면 구현] | [권민재] | [signpage.dart]

--- front/guencagaca_onwer/lib/basic/landingpage.dart
+++ front/guencagaca_onwer/lib/basic/landingpage.dart
@@ -1,16 +1,17 @@
 import 'dart:async';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:guencagaca_onwer/login/screen/loginpage.dart';
 
 class LandingPage extends StatefulWidget {
-  
+
 
   @override
   _LandingPageState createState() => _LandingPageState();
 }
 
 class _LandingPageState extends State<LandingPage> {
-  
+
   @override
   void initState() {
     Timer(Duration(seconds: 2), (){
@@ -19,7 +20,7 @@ class _LandingPageState extends State<LandingPage> {
     // TODO: implement initState
     super.initState();
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
