f9176b0c622d463aef20a23fab58e86b4a86c063
Kwon_Min_Jae <knk598500@gmail.com>
2023-10-24 11:07:34 +0900

    ✨ feat : [랜딩페이지, 색상, 레이아웃 구현] | [권민재] | [main.dart, colors.dart, default_layout.dart, landingpage.dart]

---
+++ front/guencagaca_onwer/lib/basic/landingpage.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  
+
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  
+  @override
+  void initState() {
+    Timer(Duration(seconds: 2), (){
+      Get.offAll(LoginPage());
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF8E9D7),
+      body: Center(
+        child: Image.asset("assets/mainIcon.png"),
+      ),
+    );
+  }
+}
