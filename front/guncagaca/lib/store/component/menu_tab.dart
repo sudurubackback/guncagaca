452d83e3a4b3959b5438a520c1110f68d6840463
김제준 <hoilday5303@naver.com>
2023-10-25 15:16:40 +0900

    ✏ modify : 메뉴 디테일 연동 | 김제준 | menu_tab.dart

--- front/guncagaca/lib/store/component/menu_tab.dart
+++ front/guncagaca/lib/store/component/menu_tab.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
-
+import 'package:guncagaca/menu/menu.dart';
 import '../models/menu.dart';
 
 class MenuTabWidget extends StatelessWidget {
   final List<Menu> menus;
 
-  const MenuTabWidget({super.key, required this.menus});
+  MenuTabWidget({Key? key, required this.menus}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: menus.length,
       itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: Icon(menus[index].icon),
-          title: Text(menus[index].name),
-          trailing: Text('₩${menus[index].price}'),
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DetailPage()),
+            );
+          },
+          child: ListTile(
+            leading: Icon(menus[index].icon),
+            title: Text(menus[index].name),
+            trailing: Text('₩${menus[index].price}'),
+          ),
         );
       },
     );
