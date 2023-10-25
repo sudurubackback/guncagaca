4b0101788adc59f8431f0f5e3a5a6892824ed4f4
서은진 <hara0822@naver.com>
2023-10-25 22:57:56 +0900

    Revert "Merge branch 'backend' into 'develop'"
    This reverts merge request !20

--- backend/owner-service/src/main/java/backend/sudurukbackx6/ownerservice/openAPI/service/VendorService.java
+++
@@ -1,9 +0,0 @@
-package backend.sudurukbackx6.ownerservice.openAPI.service;
-
-import backend.sudurukbackx6.ownerservice.domain.business.dto.request.VendorVailidateReqDto;
-
-import java.net.URISyntaxException;
-
-public interface VendorService {
-    int checkVendorValidation(VendorVailidateReqDto vendorVailidateReqDto) throws URISyntaxException;
-}
--- backend/store-service/src/main/java/backend/sudurukbackx6/storeservice/domain/reviews/service/ReviewService.java
+++
@@ -1,8 +0,0 @@
-package backend.sudurukbackx6.storeservice.domain.reviews.service;
-
-import backend.sudurukbackx6.storeservice.domain.reviews.service.dto.ReviewSaveRequest;
-
-public interface ReviewService {
-    void reviewSave(Long cafeId, ReviewSaveRequest request);
-    void reviewDelete(Long cafeId, Long reviewId);
-}
