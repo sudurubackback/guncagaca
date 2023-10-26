0885b5ef7e74b2ac03988f82eb95666f76787b21
Giggle1998 <rlvy98@naver.com>
2023-10-26 15:12:38 +0900

    ✨ feat : [마이페이지 이름 변경 컨트롤러 생성] | [이기표] | [MemberController]

--- backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/controller/MemberController.java
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package backend.sudurukbackx6.memberservice.domain.member.controller;
 
+import backend.sudurukbackx6.memberservice.domain.member.dto.MypageResponseDto;
 import backend.sudurukbackx6.memberservice.domain.member.dto.SignRequestDto;
 import backend.sudurukbackx6.memberservice.domain.member.dto.SignResponseDto;
 import backend.sudurukbackx6.memberservice.domain.member.service.MemberService;
@@ -25,7 +26,12 @@ public class MemberController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<SignResponseDto> refreshAccessToken(@RequestHeader("Authorization") String token) {
-        return ResponseEntity.ok(memberService.refreshAccessToken(token));
+    public ResponseEntity<SignResponseDto> refreshAccessToken(@RequestHeader("Email") String email) {
+        return ResponseEntity.ok(memberService.refreshAccessToken(email));
+    }
+
+    @PutMapping("/mypage/change-nickname")
+    public ResponseEntity<MypageResponseDto> changeNickname(@RequestHeader("Email") String email, @RequestParam String nickname) {
+        return ResponseEntity.ok(memberService.changeNickname(email, nickname));
     }
 }
