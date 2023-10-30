f33c57c75ef4785172d40ae96229d95f6fda644a
Giggle1998 <rlvy98@naver.com>
2023-10-30 17:28:29 +0900

    ✨ feat : [Owner open feign client 개발] | [이기표] | [OwnerServiceClient]

--- backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/client/OwnerServiceClient.java
+++ backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/client/OwnerServiceClient.java
@@ -1,7 +1,14 @@
 package backend.sudurukbackx6.orderservice.client;
 
+import backend.sudurukbackx6.orderservice.domain.order.dto.OrderRequestDto;
+import backend.sudurukbackx6.orderservice.domain.order.dto.OrderIndexResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("owner-service")
 public interface OwnerServiceClient {
+
+    @GetMapping("/menu/order")
+    OrderIndexResponseDto getOrder(@RequestBody OrderRequestDto orderRequestDto);
 }
