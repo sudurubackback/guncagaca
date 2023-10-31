1d9d06d8f2581b981ade9fa4f9e965bb393338c5
김제준 <hoilday5303@naver.com>
2023-10-31 14:41:08 +0900

    👶 rename : point api 이름 수정 | [김제준] | PointController.java

--- backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/points/controller/PointController.java
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/domain/points/controller/PointController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/point")
+@RequestMapping("/api/point")
 @RequiredArgsConstructor
 public class PointController {
 
