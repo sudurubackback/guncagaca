313e76fc2716b4078d09568d47657b085d9cd7ed
최영태 <dudxo7721@naver.com>
2023-11-03 04:09:10 +0900

    ✨ feat : [장바구니 조회, 주문 객체 필드 수정] | [최영태] | [cart_list, order, order_option]

---
+++ front/guncagaca/lib/order/models/order_option.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class OrderOption {
+  final String optionName;
+  final String selectedOption;
+
+  OrderOption({required this.optionName, required this.selectedOption});
+}
