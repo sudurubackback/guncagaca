821b7517b69e27b63ff8e07aeb4c1787cfeeae5a
최영태 <dudxo7721@naver.com>
2023-11-03 03:59:44 +0900

    ✨ feat : [shared_preference로 토큰 관리, 500에서 발생시 토큰 갱신 호출] | [최영태] | [dio_client, oauth_token_manager]

--- front/guncagaca/lib/common/utils/dio_client.dart
+++ front/guncagaca/lib/common/utils/dio_client.dart
@@ -1,4 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'oauth_token_manager.dart';
 
 class DioClient {
   static final Dio _dio = Dio();
@@ -19,11 +23,58 @@ class DioClient {
         onError: (DioError e, ErrorInterceptorHandler handler) {
           // 에러 발생 시에 해야 할 작업
           print("Error occurred: ${e.message}");
-          // 다른 공통적인 에러 처리 로직 추가 가능
+
+          // 500 에러 발생 시 토큰 갱신 로직
+          if (e.response?.statusCode == 500) {
+            _refreshToken().then((newToken) {
+              if (newToken != null) {
+                // 토큰을 성공적으로 갱신했을 때의 로직
+                print("재발급");
+                TokenManager().setToken(newToken);  // 갱신된 토큰 저장
+              }
+            }).catchError((error) {
+              print("Failed to refresh token: $error");
+              // 토큰 갱신 실패
+            });
+          }
           return handler.next(e);
         },
       ),
     );
     return _dio;
+  }
+
+
+  static Future<String?> getEmailFromPreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('user_email');
+  }
+
+
+  static Future<String?> _refreshToken() async {
+    String? email = await getEmailFromPreferences();
+    String baseUrl = dotenv.env['BASE_URL']!;
+
+    // 토큰 갱신 API 호출 로직
+    try {
+      print("토큰 갱신");
+      print(email);
+      print("$baseUrl/api/member/refresh");
+      final response = await _dio.post('$baseUrl/api/member/refresh',
+        options: Options(
+          headers: {
+            'Email': email,
+          },
+        ),
+        // 필요한 파라미터를 여기에 추가
+      );
+      if (response.statusCode == 200) {
+        final newToken = response.data['accessToken'];
+        return newToken;
+      }
+    } catch (error) {
+      throw Exception("Failed to refresh token: $error");
+    }
+    return null;
   }
 }
