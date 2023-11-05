c6f54d3c4155b11e32ada0553b0dcaa5b9165f8a
최영태 <dudxo7721@naver.com>
2023-11-06 02:08:31 +0900

    ✏ modify : [위치 라이브러리 변경] | [최영태] | [location_service, location_utils]

--- front/guncagaca/lib/common/utils/location_utils.dart
+++ front/guncagaca/lib/common/utils/location_utils.dart
@@ -1,6 +1,6 @@
-import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:naver_map_plugin/naver_map_plugin.dart' as naver;
 
-naver.LatLng convertToLatLng(LocationData data) {
-  return naver.LatLng(data.latitude!, data.longitude!);
+naver.LatLng naverConvertToLatLng(Position? data) {
+  return naver.LatLng(data!.latitude!, data!.longitude!);
 }
