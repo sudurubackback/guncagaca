70efad88e794601b42ba6a4253292a9edfbc0520
최영태 <dudxo7721@naver.com>
2023-11-07 01:11:32 +0900

    ✏ modify : [내 리뷰 조회 디버그, 리뷰 최대길이 설정] | [최영태] | [my_review, review_list]

---
+++ front/guncagaca/lib/myreview/my_review.dart
@@ -0,0 +1,34 @@
+import 'package:intl/intl.dart';
+
+class MyReview {
+  final int reviewId;
+  final int storeId;
+  final String storeName;
+  final String img;
+  final String comment;
+  final double starTotal;
+  final double star;
+
+  MyReview({
+    required this.storeId,
+    required this.storeName,
+    required this.img,
+    required this.reviewId,
+    required this.comment,
+    required this.starTotal,
+    required this.star
+  });
+
+  factory MyReview.fromMap(Map<String, dynamic> map) {
+    return MyReview(
+      reviewId: map['id'],
+      storeName: map['storeName'],
+      storeId: map['storeId'],
+      img: map['img'],
+      comment: map['comment'],
+      starTotal: map['starTotal'],
+      star: map['star'],
+    );
+  }
+
+}
