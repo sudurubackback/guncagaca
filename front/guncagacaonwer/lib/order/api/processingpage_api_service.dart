bcabd41744930c7628f13510d748e4c5d5c17848
Kwon_Min_Jae <knk598500@gmail.com>
2023-11-07 21:56:00 +0900

    ✨ feat : [주문 완료 API 구현] | [권민재] | [ordercomplete]

--- front/guncagacaonwer/lib/order/api/processingpage_api_service.dart
+++ front/guncagacaonwer/lib/order/api/processingpage_api_service.dart
@@ -1,3 +1,4 @@
+import 'package:guncagacaonwer/order/models/ordercompletemodel.dart';
 import 'package:guncagacaonwer/order/models/orderlistmodel.dart';
 import 'package:guncagacaonwer/store/models/ownerinfomodel.dart';
 import 'package:retrofit/retrofit.dart';
@@ -9,12 +10,18 @@ part 'processingpage_api_service.g.dart';
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
+  // 주문 목록 리스트
   @GET("/api//list/{storeId}/{status}")
   Future<List<Order>> getProcessingList(
       @Path("storeId") int storeId,
       @Path("status") String status,
       );
 
+  // 사장님 정보
   @GET("/api/ceo/ownerInfo")
   Future<OwnerInfoResponse> getOwnerInfo(@Header("Authorization") String token);
+
+  // 주문 완료 요청
+  @POST("/api/order/complete/{orderId}")
+  Future<OrderCompleteResponse> completeOrder(@Header("Authorization") String token, @Path("orderId") String orderId);
 }
