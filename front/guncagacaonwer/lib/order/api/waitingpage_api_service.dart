247ade6ed5c790aed045c39dfc3a7395eff27436
Kwon_Min_Jae <knk598500@gmail.com>
2023-11-07 23:13:49 +0900

    🛠 fix : [build 실행] | [권민재] | [api_service all]

--- front/guncagacaonwer/lib/order/api/waitingpage_api_service.dart
+++ front/guncagacaonwer/lib/order/api/waitingpage_api_service.dart
@@ -10,19 +10,23 @@ part 'waitingpage_api_service.g.dart';
 @RestApi(baseUrl : "https://k9d102.p.ssafy.io")
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
-  
+
+  // 주문 접수
   @POST("/api/order/request/{orderId}")
   Future<OrderRequestResponse> requestOrder(@Header("Email") String email, @Path("orderId") String orderId);
-  
+
+  // 목록 리스트
   @GET("/api/list/{storeId}/{status}")
   Future<List<Order>> getWaitingList(
       @Path("storeId") int storeId,
       @Path("status") String status,
   );
 
+  // 사장님 정보
   @GET("/api/ceo/ownerInfo")
   Future<OwnerInfoResponse> getOwnerInfo(@Header("Authorization") String token);
 
+  // 주문 취소
   @POST("/api/order/cancel")
   Future<String> cancelOrder(@Header("Email") String email, @Body() OrderCancelRequest request);
 }
