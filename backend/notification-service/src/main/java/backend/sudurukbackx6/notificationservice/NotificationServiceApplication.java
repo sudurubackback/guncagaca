6d670d82ae242e203974a29726bac5f7cb896a56
silver <hara0822@naver.com>
2023-11-08 14:49:25 +0900

    🛠 fix : [swagger 설정] | [서은진] | [notification]

--- backend/notification-service/src/main/java/backend/sudurukbackx6/notificationservice/NotificationServiceApplication.java
+++ backend/notification-service/src/main/java/backend/sudurukbackx6/notificationservice/NotificationServiceApplication.java
@@ -5,7 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+				org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+		}
+)
 @EnableDiscoveryClient
 @EnableFeignClients
 public class NotificationServiceApplication {
