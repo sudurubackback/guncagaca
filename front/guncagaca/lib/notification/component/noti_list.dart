cdf08a20e9a36dae92604f81cd4c375d24b467e5
김제준 <hoilday5303@naver.com>
2023-11-08 16:58:27 +0900

    ✏ modify : 알림함 통신 구현 | [김제준] | noti_list.dart

--- front/guncagaca/lib/notification/component/noti_list.dart
+++ front/guncagaca/lib/notification/component/noti_list.dart
@@ -27,114 +27,82 @@ class _NotiListState extends State<NotiList> {
 
 
   Future<void> test() async {
-    final String apiUrl = "http://k9d102.p.ssafy.io:8082/v1/fcm/sendMember";
+    final String apiUrl = "http://k9d102.p.ssafy.io:8082/api/alert/history";
 
-    final response = await dio.post(
+    final response = await dio.get(
         apiUrl,
         options: Options(
             headers: {
               'Authorization': "Bearer $token",
             }
         ),
-        data: {
-          "memberId": 5,
-          "title": "string",
-          "body": "string",
-          "imageUrl": "string",
-          "productCode": "string"
-        }
+
     );
     print(response.data);
     if (response.statusCode == 200) {
-          print("성공");
+      List<dynamic> jsonData = response.data;
+      notifications = List<Map<String, dynamic>>.from(jsonData);
+      print(notifications);
+      setState(() {});
     } else {
       throw Exception("Failed to fetch menus.");
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
+    print('All points: ${notifications}');
+
     return notifications.isEmpty
         ? Center(
       child: Text(
-        "알림이 없습니다.",
+        "알림함이 비었습니다.",
         style: TextStyle(fontSize: 18.0),
       ),
     )
         : ListView.builder(
-      itemCount: notifications.length + 1,
+      itemCount: notifications.length + 1, // 한 개를 추가합니다.
       itemBuilder: (BuildContext context, int index) {
         if (index == notifications.length) {
-          return SizedBox(height: 20);
+          return SizedBox(height: 20); // 마지막 아이템일 때 공간을 추가합니다.
         }
+
         return Container(
           margin: EdgeInsets.only(top: 15, left: 10, right: 10),
           decoration: BoxDecoration(
             border: Border.all(
-              color: Color(0xff9B5748),
+              color: PRIMARY_COLOR,
               width: 1.0,
             ),
             borderRadius: BorderRadius.circular(20.0),
           ),
           child: ListTile(
-            contentPadding: EdgeInsets.symmetric(vertical: 13.0, horizontal: 16.0),
+            contentPadding:
+            EdgeInsets.symmetric(vertical: 13.0, horizontal: 16.0),
             title: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: 60,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-
-                          ),
-                        ),
-                        SizedBox(width: 10),
-
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.star, color: Colors.amber, size: 25),
-                        SizedBox(width: 10),
-                       ],
-                    ),
-                  ],
+                Text(
+                  notifications[index]['title'],
+                  style: TextStyle(
+                      fontSize: 15.0, fontWeight: FontWeight.bold),
                 ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Icon(Icons.chat, color: Colors.grey, size: 20),
-                    SizedBox(width: 5),
-                    Expanded(
-                      child: Container(
-                        padding: EdgeInsets.all(10.0),
-                        decoration: BoxDecoration(
-                          color: Color(0xffF8E9D7),
-                          border: Border.all(color: Color(0xff9B5748), width: 2.0),
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        child: Row(
-                          children: [
-
-                            Icon(Icons.star, color: Colors.amber, size: 18),
-                            SizedBox(width: 3),
-
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+                SizedBox(height: 8.0),
+                Text(
+                  notifications[index]['body'],
+                  style: TextStyle(fontSize: 18.0),
                 ),
               ],
             ),
+            trailing: GestureDetector(
+              onTap: () {
+                // _removeNotification(index);
+              },
+              child: Icon(Icons.close),
+            ),
             onTap: () {
-
+              // 알림을 눌렀을 때의 동작을 추가하세요.
             },
           ),
         );
@@ -142,3 +110,4 @@ class _NotiListState extends State<NotiList> {
     );
   }
 }
+
