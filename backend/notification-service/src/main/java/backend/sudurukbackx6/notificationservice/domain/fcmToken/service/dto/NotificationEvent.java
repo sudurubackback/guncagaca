bed1a1802038aac126d26651afb43233494932aa
최영태 <dudxo7721@naver.com>
2023-11-13 01:14:22 +0900

    ✨ feat : [알림 서비스 kafka 설정] | [최영태] | [kafkaConfig, NotificationEvent]

---
+++ backend/notification-service/src/main/java/backend/sudurukbackx6/notificationservice/domain/fcmToken/service/dto/NotificationEvent.java
@@ -0,0 +1,16 @@
+package backend.sudurukbackx6.notificationservice.domain.fcmToken.service.dto;
+
+import backend.sudurukbackx6.notificationservice.domain.fcmToken.entity.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NotificationEvent {
+
+    private Long memberId;
+    private Status status;
+    private Long storeId;
+    private String storeName;
+    private String reason;
+}
