d01d5ad23954a7e539aa1eb9f0e8319b605646b2
최영태 <dudxo7721@naver.com>
2023-11-13 01:17:12 +0900

    ✨ feat : [알림 히스토리 저장] | [최영태] | [AlertHistoryService, AlertHistory, ]

--- backend/notification-service/src/main/java/backend/sudurukbackx6/notificationservice/domain/fcmToken/service/AlertHistoryService.java
+++ backend/notification-service/src/main/java/backend/sudurukbackx6/notificationservice/domain/fcmToken/service/AlertHistoryService.java
@@ -5,6 +5,9 @@ import backend.sudurukbackx6.notificationservice.domain.fcmToken.client.dto.resp
 import backend.sudurukbackx6.notificationservice.domain.fcmToken.service.dto.AlertHistoryDto;
 import backend.sudurukbackx6.notificationservice.domain.fcmToken.entity.AlertHistory;
 import backend.sudurukbackx6.notificationservice.domain.fcmToken.repository.AlertHistoryRepository;
+import backend.sudurukbackx6.notificationservice.domain.fcmToken.service.dto.NotificationDto;
+import backend.sudurukbackx6.notificationservice.domain.fcmToken.service.dto.NotificationEvent;
+import com.google.firebase.messaging.Notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.expression.ExpressionException;
 import org.springframework.stereotype.Service;
@@ -17,6 +20,20 @@ import java.util.List;
 public class AlertHistoryService {
 	private final AlertHistoryRepository alertHistoryRepository;
 	private final MemberFeignClient memberFeignClient;
+
+	// 알림 기록 생성
+	public void saveAlertHistory(NotificationEvent notificationEvent, NotificationDto notificationDto) {
+
+		// 알림 내역 저장
+		AlertHistory alertHistory = AlertHistory.builder()
+				.title(notificationDto.getTitle())
+				.body(notificationDto.getBody())
+				.memberId(notificationEvent.getMemberId())
+				.storeId(notificationEvent.getStoreId())
+				.build();
+
+		alertHistoryRepository.save(alertHistory);
+	}
 
 	// 알림 목록 조회
 	public List<AlertHistoryDto> getAlertHistory(String token) {
@@ -35,32 +52,27 @@ public class AlertHistoryService {
 
 			alertHistoryDtos.add(alertHistoryDto);
 		}
-
 		return alertHistoryDtos;
-
-
 	}
 
-// 알림 삭제
-public void deleteAlertHistory(String token, Long alertId) {
-	MemberInfoResponse memberInfo = memberFeignClient.getMemberInfo(token);
-	Long memberId = memberInfo.getId();
+	// 알림 삭제
+	public void deleteAlertHistory(String token, Long alertId) {
+		MemberInfoResponse memberInfo = memberFeignClient.getMemberInfo(token);
+		Long memberId = memberInfo.getId();
 
-	// 알림 기록 가져오기
-	AlertHistory alertHistory = alertHistoryRepository.findById(alertId)
-			.orElseThrow(() -> new RuntimeException("해당 알림 기록을 찾을 수 없습니다."));
+		// 알림 기록 가져오기
+		AlertHistory alertHistory = alertHistoryRepository.findById(alertId)
+				.orElseThrow(() -> new RuntimeException("해당 알림 기록을 찾을 수 없습니다."));
 
-	// 본인 아닌경우
-	if (!alertHistory.getMemberId().equals(memberId)) {
-		throw new RuntimeException("해당 알림을 삭제할 권한이 없습니다.");
+		// 본인 아닌경우
+		if (!alertHistory.getMemberId().equals(memberId)) {
+			throw new RuntimeException("해당 알림을 삭제할 권한이 없습니다.");
+		}
+
+		// 삭제
+		alertHistoryRepository.delete(alertHistory);
 	}
 
-	// 삭제
-	alertHistoryRepository.delete(alertHistory);
-}
-
-
-//
 //	// 알림 전체 삭제
 //	public void deleteAllAlertHistory(Long memberId) {
 //		// 현재 유저 알림 목록
