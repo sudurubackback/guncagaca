944057b7ee3621615a82fbcb50e5af88e820e8e1
Giggle1998 <rlvy98@naver.com>
2023-11-14 02:16:10 +0900

    👶 rename : [vendor 패키지 수정] | [이기표] | [vendor]

--- backend-cafe/owner-service/src/main/java/backend/sudurukbackx6/ownerservice/openAPI/service/VendorService.java
+++
@@ -1,9 +0,0 @@
-package backend.sudurukbackx6.ownerservice.openAPI.service;
-
-import backend.sudurukbackx6.ownerservice.domain.business.dto.request.VendorValidateReqDto;
-
-import java.net.URISyntaxException;
-
-public interface VendorService {
-    int checkVendorValidation(VendorValidateReqDto vendorVailidateReqDto) throws URISyntaxException;
-}
