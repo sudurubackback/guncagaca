7cbb6ece974321096498338a688efdb7f9d5c47b
Giggle1998 <rlvy98@naver.com>
2023-11-13 10:54:32 +0900

    ⚙ config : [store-service kafka 설정] | [이기표] | [application.yml]

--- backend-cafe/store-service/src/main/java/backend/sudurukbackx6/storeservice/global/config/KafkaConfig.java
+++ backend-cafe/store-service/src/main/java/backend/sudurukbackx6/storeservice/global/config/KafkaConfig.java
@@ -1,17 +1,16 @@
 package backend.sudurukbackx6.storeservice.global.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -30,12 +29,7 @@ public class KafkaConfig {
         return new KafkaAdmin(configs);
     }
 
-    // 테스트
-    @Bean
-    public NewTopic myTopic1() {
-        return new NewTopic("my_topic_1", 1, (short) 1);
-    }
-
+    // 이벤트 발행 부분
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
@@ -50,4 +44,22 @@ public class KafkaConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 
+    // 이벤트 구독 부분
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "order");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
 }
