45f474f13be6e9dcc1e8cc85c9d5eb1f256ae866
Giggle1998 <rlvy98@naver.com>
2023-11-14 02:17:36 +0900

    🛠 fix : [빌드 에러] | [이기표] | [OwnerStatistics]

--- backend-cafe/owner-service/src/main/java/backend/sudurukbackx6/ownerservice/domain/owner/controller/OwnerStatisticController.java
+++ backend-cafe/owner-service/src/main/java/backend/sudurukbackx6/ownerservice/domain/owner/controller/OwnerStatisticController.java
@@ -20,39 +20,39 @@ import java.util.List;
 @RequestMapping("/api/ceo")
 public class OwnerStatisticController {
 
-    private final OwnerStatisticsService ownerStatisticsService;
+//    private final OwnerStatisticsService ownerStatisticsService;
 
-    @GetMapping("/store/{storeId}/orders")
-    public List<StoreOrderResponse> getStoreOrdersForDateRange(
-            @PathVariable Long storeId,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
-
-        if (startDate == null && endDate == null) {
-            // 두 날짜가 모두 누락된 경우 현재 날짜 등을 사용하거나 기본 범위를 설정할 수 있습니다.
-            // 예: 오늘의 주문만 가져오기
-            startDate = LocalDate.now();
-            endDate = LocalDate.now();
-        } else if (startDate == null || endDate == null) {
-            // 하나의 날짜만 누락된 경우 에러를 반환하거나, 기본값을 설정할 수 있습니다.
-            throw new IllegalArgumentException("Both startDate and endDate are required if one is provided.");
-        }
-
-        // 날짜 범위에 해당하는 주문을 가져오는 서비스 메소드 호출
-        return ownerStatisticsService.getStoreOrdersForDateRange(storeId, startDate, endDate);
-    }
-
-    @GetMapping("/store/{storeId}/orders/summary")
-    public ResponseEntity<backend.sudurukbackx6.ownerservice.domain.owner.dto.SalesSummaryResponse> getSalesSummary(
-            @PathVariable Long storeId,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
-
-        if (startDate == null || endDate == null) {
-            return ResponseEntity.badRequest().body(null);
-        }
-
-        backend.sudurukbackx6.ownerservice.domain.owner.dto.SalesSummaryResponse summary = ownerStatisticsService.getSalesSummary(storeId, startDate, endDate);
-        return ResponseEntity.ok(summary);
-    }
+//    @GetMapping("/store/{storeId}/orders")
+//    public List<StoreOrderResponse> getStoreOrdersForDateRange(
+//            @PathVariable Long storeId,
+//            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+//            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+//
+//        if (startDate == null && endDate == null) {
+//            // 두 날짜가 모두 누락된 경우 현재 날짜 등을 사용하거나 기본 범위를 설정할 수 있습니다.
+//            // 예: 오늘의 주문만 가져오기
+//            startDate = LocalDate.now();
+//            endDate = LocalDate.now();
+//        } else if (startDate == null || endDate == null) {
+//            // 하나의 날짜만 누락된 경우 에러를 반환하거나, 기본값을 설정할 수 있습니다.
+//            throw new IllegalArgumentException("Both startDate and endDate are required if one is provided.");
+//        }
+//
+//        // 날짜 범위에 해당하는 주문을 가져오는 서비스 메소드 호출
+//        return ownerStatisticsService.getStoreOrdersForDateRange(storeId, startDate, endDate);
+//    }
+//
+//    @GetMapping("/store/{storeId}/orders/summary")
+//    public ResponseEntity<backend.sudurukbackx6.ownerservice.domain.owner.dto.SalesSummaryResponse> getSalesSummary(
+//            @PathVariable Long storeId,
+//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+//            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+//
+//        if (startDate == null || endDate == null) {
+//            return ResponseEntity.badRequest().body(null);
+//        }
+//
+//        backend.sudurukbackx6.ownerservice.domain.owner.dto.SalesSummaryResponse summary = ownerStatisticsService.getSalesSummary(storeId, startDate, endDate);
+//        return ResponseEntity.ok(summary);
+//    }
 }
