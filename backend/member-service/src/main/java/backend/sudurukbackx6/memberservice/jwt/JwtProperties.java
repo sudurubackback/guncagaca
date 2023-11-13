1be5a93d874d781bc66a38bcceb3c6d823bc7952
silver <hara0822@naver.com>
2023-11-13 17:19:32 +0900

    ⚙ config : [jwt security 변경] | [서은진] | [member : jwt]

---
+++ backend/member-service/src/main/java/backend/sudurukbackx6/memberservice/jwt/JwtProperties.java
@@ -0,0 +1,17 @@
+package backend.sudurukbackx6.memberservice.jwt;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+@Setter
+@Getter
+public class JwtProperties {
+    String header;
+    String secret;
+    String issuer;
+}
