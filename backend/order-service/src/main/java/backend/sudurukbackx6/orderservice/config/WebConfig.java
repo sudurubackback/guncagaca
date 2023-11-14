77319326e6fa1d86a06a656fbc56fb46bf28a722
Giggle1998 <rlvy98@naver.com>
2023-11-15 00:14:36 +0900

    ✏ modify : [cors 수정] | [이기표] | [WebConfig]

--- backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/config/WebConfig.java
+++ backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://127.0.0.1:8080");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
