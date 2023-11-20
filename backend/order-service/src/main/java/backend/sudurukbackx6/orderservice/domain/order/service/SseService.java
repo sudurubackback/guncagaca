c66902e9dd992e7dd5701ef3d225a964a883035f
최영태 <dudxo7721@naver.com>
2023-11-20 22:35:18 +0900

    🛠 fix : [sse 로그] | [최영태] | [SseService]

--- backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/domain/order/service/SseService.java
+++ backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/domain/order/service/SseService.java
@@ -1,5 +1,6 @@
 package backend.sudurukbackx6.orderservice.domain.order.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -11,6 +12,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 @Service
+@Slf4j
 public class SseService {
 
     private final Map<Long, SseEmitter> emitterMap = new ConcurrentHashMap<>();
@@ -33,15 +35,18 @@ public class SseService {
     }
 
     public SseEmitter createEmitter(Long storeId) {
+        log.info("sse 등록 {}", storeId);
         SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
         emitterMap.put(storeId, emitter);
         return emitter;
     }
 
     public void sendToStoreClients(Long storeId, Object data) {
+        log.info("주문 생성됨");
         SseEmitter emitter = emitterMap.get(storeId);
         if(emitter != null) {
             try {
+                log.info("주문 전송");
                 emitter.send(data);
             } catch (IOException e) {
                 emitterMap.remove(storeId);
