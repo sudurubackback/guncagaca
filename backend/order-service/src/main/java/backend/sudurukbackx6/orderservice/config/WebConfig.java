b310cf32fc3fa6b370c71523cec3f985befb93a5
최영태 <dudxo7721@naver.com>
2023-11-20 16:49:52 +0900

    webconfig 수정 | [김제준] | [webconfig]

--- backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/config/WebConfig.java
+++ backend/order-service/src/main/java/backend/sudurukbackx6/orderservice/config/WebConfig.java
@@ -9,9 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://hoilday5303.store") // 허용할 도메인을 명시
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드를 명시
-                .allowCredentials(true); // 필요에 따라 쿠키 전송을 허용
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
